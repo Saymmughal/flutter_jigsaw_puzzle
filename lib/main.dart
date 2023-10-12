98c6a8a8b331d6fe6dc26b8308ea8baf0ee67ed0
Saim <Saim@codesorbit.com>
2023-10-12 18:14:23 +0500

    seprate puzzle view for package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:jigsaw_puzzle/home_page.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:jigsaw_puzzle/puzzle_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +21,100 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Jigsaw Puzzle'),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  File? _image;
+  GlobalKey<PuzzleViewState> puzzleKey = GlobalKey<PuzzleViewState>();
+  Future getImage(ImageSource source) async {
+    var image = await ImagePicker().pickImage(
+        source: source, maxHeight: 1000, maxWidth: 1000, imageQuality: 50);
+
+    if (image != null) {
+      setState(() {
+        _image = File(image.path);
+      });
+      puzzleKey.currentState!.generatePuzzle(_image!);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child:
+              Column(
+            children: [
+              PuzzleView(
+                key: puzzleKey,
+                image: _image,
+                height: 400,
+                width: MediaQuery.of(context).size.width,
+                onCompleted: () {},
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Center(
+                  child: InkWell(
+                onTap: () {
+                  puzzleKey.currentState!.resetPuzzle();
+                },
+                child: const Icon(
+                  Icons.refresh,
+                  size: 50,
+                ),
+              )),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet<void>(
+              context: context,
+              builder: (BuildContext context) {
+                return SafeArea(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      ListTile(
+                        leading: const Icon(Icons.camera),
+                        title: const Text('Camera'),
+                        onTap: () {
+                          getImage(ImageSource.camera);
+                          Navigator.pop(context);
+                        },
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.image),
+                        title: const Text('Gallery'),
+                        onTap: () {
+                          getImage(ImageSource.gallery);
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              });
+        },
+        tooltip: 'New Image',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
